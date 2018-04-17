a70f84e382c1e2fa7e3d483ac7cf7d9a4e392af3
Cian Synnott <cian@emauton.org>
2018-04-17 21:20:28 +0100

    Satisfy lint

--- hg_agent_periodic/periodic.py
+++ hg_agent_periodic/periodic.py
@@ -236,7 +236,7 @@ def config_once(args):
 
     if old_config:
         if (config_changed(old_config, agent_config, 'endpoint_url') or
-            config_changed(old_config, agent_config, 'api_key')):
+                config_changed(old_config, agent_config, 'api_key')):
             restart_process(args, 'forwarder')
 
     new_diamond = gen_diamond_config(agent_config)
